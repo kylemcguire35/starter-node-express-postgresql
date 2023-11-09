cc8a4f77b964e1bbef889ec3871f0e065a17799d
Kyle <kylemcguire35@gmail.com>
2023-11-08 22:05:58 -0800

    completed lessons

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,13 +1,11 @@
-async function list(req, res, next) {
-  res.json({
-    data: [
-      { category_name: "category 1" },
-      { category_name: "category 2" },
-      { category_name: "category 3" },
-    ],
-  });
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const categoriesService = require("./categories.service");
+
+async function list(req, res) {
+  const data = await categoriesService.list();
+  res.json({ data });
 }
 
 module.exports = {
-  list: [list],
+  list: asyncErrorBoundary(list),
 };
